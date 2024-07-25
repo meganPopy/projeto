211d405b9172d044a33c1e85924092c511ddc58e
Tânia <106488757+meganPopy@users.noreply.github.com>
2024-07-25 00:08:19 -0300

    a

---
+++ main.js
@@ -0,0 +1,7 @@
+const primeiroNumero = 1; 
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero/numeroPontoFlutuante; 
+
+console.console.log(novaOperacao);
